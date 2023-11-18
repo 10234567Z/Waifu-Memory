62e91d0f09c6bfbf9874aacde557b6d66a4561c0
10234567Z <93607971+10234567Z@users.noreply.github.com>
2023-11-18 12:51:42 +0530

    Add vite react template + Add react template of vite ,then reset the styles and default webpage then add a basic Header in app.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState , useEffect } from 'react'
+import './Styles/App.css'
+import Header from './Components/header'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/header.jsx
@@ -0,0 +1,10 @@
+export default function Header(){
+    return (
+        <header>
+            <h1>Waifu Memories</h1>
+            <i>
+                <h3>Let's test how well you remember your waifus</h3>
+            </i>
+        </header>
+    )
+}
