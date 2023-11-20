78e76d346090b1ad38dcbcfcf578fbc954d1a885
10234567Z <93607971+10234567Z@users.noreply.github.com>
2023-11-20 18:36:05 +0530

    Display the requested characters

--- src/Components/Main Components/cards.jsx
+++ src/Components/Main Components/cards.jsx
@@ -13,19 +13,25 @@ export default function Cards() {
     const url = "https://waifu.it/api/waifu";
     async function GetWaifu() {
         try {
-            for (let i = 0; i < 30; i++) {
+            for (let i = 0; i < 15; i++) {
                 const { data } = await axios.get(url, {
                     headers: {
                         Authorization: "OTc4NjI0OTc5ODkxNTM1OTAz.MTcwMDIwMTAyNw--.3fc54432151",
                     },
                 });
-                setImgLinks((prev) => [...prev, data.images[0]]);
-                setCNames((prev) => [...prev, data.names.en]);
-                if (i === 29) {
-                    setLoaded(true);
+
+                if(cNames.includes(data.names.en)){
+                    i--;
                 }
-                setProgress(i + 1)
-                await new Promise((resolve) => setTimeout(resolve, 500));
+                else{
+                    setImgLinks((prev) => [...prev, data.images[0]]);
+                    setCNames((prev) => [...prev, data.names.en]);
+                    if (i === 14) {
+                        setLoaded(true);
+                    }
+                    setProgress(i + 1)
+                    await new Promise((resolve) => setTimeout(resolve, 500));
+                }
             }
         } catch (err) {
             throw new Error(err.message);
@@ -38,17 +44,25 @@ export default function Cards() {
 
     return (
         <>
-            {loaded
-                ?
-                <>
-                    <section className="cards">Loaded</section>
-                </>
-                :
-                <>
-                    <h2>Loading Your Waifies....</h2>
-                    <progress value={progress} max="30" ></progress>
-                    <img src={loading} height='500px' width='500px' alt="loading.."></img>
-                </>
+            {
+                loaded
+                    ?
+                    <section className="cards">
+                        {Array.from({ length: 30 }, (_, i) => (
+                            <div className="card" key={i}>
+                                <div className="image">
+                                    <img src={imgLinks[i]} alt="cardImage" width="200px" height="200px"></img>
+                                </div>
+                                <h3>{cNames[i]}</h3>
+                            </div>
+                        ))}
+                    </section>
+                    :
+                    <>
+                        <h2>Loading Your Waifies....</h2>
+                        <progress value={progress} max="15" ></progress>
+                        <img src={loading} height='500px' width='500px' alt="loading.."></img>
+                    </>
             }
         </>
     )
