2db11865eb87c68346ea39d67aee7426abf1845f
10234567Z <93607971+10234567Z@users.noreply.github.com>
2023-11-20 19:18:21 +0530

    Prevent same character to be saved

--- src/Components/Main Components/cards.jsx
+++ src/Components/Main Components/cards.jsx
@@ -11,28 +11,32 @@ export default function Cards() {
     const [cNames, setCNames] = useState([])
     const [progress, setProgress] = useState(0)
     const url = "https://waifu.it/api/waifu";
-    async function GetWaifu() {
-        try {
-            for (let i = 0; i < 30; i++) {
-                const { data } = await axios.get(url, {
-                    headers: {
-                        Authorization: "OTc4NjI0OTc5ODkxNTM1OTAz.MTcwMDIwMTAyNw--.3fc54432151",
-                    },
-                });
 
-                if(cNames.includes(data.names.en)){
-                    i--;
-                }
-                else{
-                    setImgLinks((prev) => [...prev, data.images[0]]);
-                    setCNames((prev) => [...prev, data.names.en]);
-                    if (i === 29) {
-                        setLoaded(true);
-                    }
-                    setProgress(i + 1)
-                    await new Promise((resolve) => setTimeout(resolve, 500));
-                }
-            }
+    async function fetchWaifu() {
+        const response = await axios.get(url, {
+            headers: {
+                Authorization: "OTc4NjI0OTc5ODkxNTM1OTAz.MTcwMDIwMTAyNw--.3fc54432151",
+            },
+        });
+        return response.data;
+    }
+    async function GetWaifu() {
+        const uniqueNames = new Set(cNames);
+        try {
+            const promises = Array.from({ length: 20 }, async (_, i) => {
+                let data;
+                do {
+                    data = await fetchWaifu();
+                } while (uniqueNames.has(data.names.en));
+                setProgress(i + 1);
+                return data;
+            });
+            let results = await Promise.all(promises)
+            results.forEach((data, i) => {
+                setImgLinks((prev) => [...prev, data.images[0]]);
+                setCNames((prev) => [...prev, data.names.en]);
+            });
+            setLoaded(true)
         } catch (err) {
             throw new Error(err.message);
         }
@@ -48,7 +52,7 @@ export default function Cards() {
                 loaded
                     ?
                     <section className="cards">
-                        {Array.from({ length: 30 }, (_, i) => (
+                        {Array.from({ length: 20 }, (_, i) => (
                             <div className="card" key={i}>
                                 <div className="image">
                                     <img src={imgLinks[i]} alt="cardImage" width="200px" height="200px"></img>
@@ -60,7 +64,7 @@ export default function Cards() {
                     :
                     <>
                         <h2>Loading Your Waifies....</h2>
-                        <progress value={progress} max="30" ></progress>
+                        <progress value={progress} max="20" ></progress>
                         <img src={loading} height='500px' width='500px' alt="loading.."></img>
                     </>
             }
